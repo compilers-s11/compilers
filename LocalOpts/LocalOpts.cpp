1b15113be81b72460ae7a83b4403dc363de6f1af
Cyrus Omar <cyology@gmail.com>
2011-01-19 12:32:21 -0500

    Added LocalOpts.

---
+++ LocalOpts/LocalOpts.cpp
@@ -0,0 +1,30 @@
+#include "llvm/Pass.h"
+#include "llvm/BasicBlock.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <ostream>
+
+using namespace llvm;
+
+namespace
+{
+
+	struct LocalOpts : public BasicBlockPass
+	{
+		static char ID;
+		LocalOpts : BasicBlockPass(ID) {}
+		
+		virtual bool runOnBasicBlock(BasicBlock &bb) {
+			bool modified = false;
+			// Iterate over instructions
+			for (BasicBlock::iterator i = bb.begin(), e = bb.end(); i != e; ++i)
+			{
+				errs() << *i << "\n";
+			}
+			return modified;
+		}
+	};
+	
+	char Hello::ID = 0;
+	static RegisterPass x("LocalOpts", "LocalOpts", false, false);
+}
