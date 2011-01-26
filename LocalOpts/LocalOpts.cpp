2be7ed13b0a21918a9975b8f9c805ea640f7c88e
Cyrus Omar <cyology@gmail.com>
2011-01-26 13:44:09 -0500

    Changes from 1/26 morning mtg.

--- LocalOpts/LocalOpts.cpp
+++ LocalOpts/LocalOpts.cpp
@@ -1,6 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/BasicBlock.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Instruction.h"
+#include "llvm/Constants.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #include <ostream>
 
@@ -19,7 +22,46 @@ namespace
 			// Iterate over instructions
 			for (BasicBlock::iterator i = bb.begin(), e = bb.end(); i != e; ++i)
 			{
-				errs() << *i << "\n";
+				Value* L = i->getOperand(0);
+				Value* R = i->getOperand(1);
+
+			    switch (i->getOpcode()) {
+					default:
+						
+						break;
+					case Instruction::Add:
+						// Algebraic identities
+						if (ConstantInt *LC = dyn_cast<ConstantInt>(L)) {
+							if (LC->isZero()) {
+								ReplaceInstWithValue(i->getParent()->getInstList(), i, R);
+								modified = true;
+							}
+						}
+						break;
+					case Instruction::FAdd:
+					case Instruction::Sub:
+					case Instruction::FSub:
+					case Instruction::Mul:
+					case Instruction::FMul:
+					case Instruction::UDiv:
+					case Instruction::SDiv:
+					case Instruction::FDiv:
+					case Instruction::URem:
+					case Instruction::SRem:
+					case Instruction::FRem:
+					case Instruction::Shl:						
+					case Instruction::LShr:						
+					case Instruction::AShr:					
+					case Instruction::And:					
+					case Instruction::Or:
+						
+						break;
+						
+					case Instruction::Xor:
+						
+						break;
+			    }
+				//errs() << *i << "\n";
 			}
 			return modified;
 		}
