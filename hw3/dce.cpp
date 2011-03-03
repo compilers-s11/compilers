c1615027ece7e630a68d261d37acbea5fab0dfb0
hellfeuer <hellfeuer@gmail.com>
2011-03-03 04:56:31 -0500

    Report and code comments

--- hw3/dce.cpp
+++ hw3/dce.cpp
@@ -34,10 +34,10 @@ namespace
           instIn = new ValueMap<Instruction*, BitVector*>();
         }
 
-        // map from instructions/argument to their index in the bitvector
+        // Map from instructions/argument to their index in the bitvector
         std::map<Value*, int> *index;
         
-        // map from index in bitvector back to instruction/argument
+        // Map from index in bitvector back to instruction/argument
         std::vector<Value*> *r_index;
         
         // convenience
@@ -58,14 +58,13 @@ namespace
         }
         
 
-        //This is probably sufficient. isa<Terminator> is wrong because of invoke
+        // An instruction is defines a variable iff its type is not void
         bool isDefinition(Instruction *ii) {
           return (!ii->getType()->isVoidTy());
         }
-        //bool isDefinition(Instruction *ii) {
-        //  return (!(isa<TerminatorInst>(ii) || isa<StoreInst>(ii) || (isa<CallInst>(ii) && ii->getType()->isVoidTy())));
-        //}
 
+        // Terminators and Calls can never be removed from the function due to possible side effects
+        // Stores can sometimes be removed, but they form a special case in the function Eliminate
         bool isEliminableDef(Instruction *ii) {
           return (!(isa<TerminatorInst>(ii) || isa<StoreInst>(ii) || isa<CallInst>(ii)));
         }
@@ -79,7 +78,7 @@ namespace
           numTotal = 0;
           numArgs = 0;
           
-          // add function arguments to maps
+          // Add function arguments to maps
           for (Function::arg_iterator ai = F.arg_begin(), ae = F.arg_end(); ai != ae; ai++) {
             (*index)[&*ai] = numArgs;
             r_index->push_back(&*ai);
@@ -88,7 +87,7 @@ namespace
           numTotal = numArgs; 
 
           
-          // add definitions to maps
+          // Add definitions to maps
           for (inst_iterator ii = inst_begin(&F), ie = inst_end(&F); ii != ie; ii++) {
             if (isDefinition(&*ii)) {
               (*index)[&*ii] = numTotal;
@@ -97,61 +96,42 @@ namespace
             }
           }
           
-          // initialize instIn
+          // Initialize instIn
           for (inst_iterator ii = inst_begin(&F), ie = inst_end(&F); ii != ie; ii++) {
             (*instIn)[&*ii] = new BitVector(numTotal, true); //TODO true orfalse
           }
           top = new BitVector(numTotal, true);
           
-          // run data flow 
+          // Run data flow 
           Dataflow<false>::runOnFunction(F);
 
-          //true if something was modified
+          // Eliminate returns true if any instruction was removed
           return Eliminate(F);
         }
         
         virtual BitVector* transfer(BasicBlock& bb) {
-          // we iterate over instructions in reverse beginning with out[bb]
+          // We iterate over instructions in reverse beginning with out[bb]
           BitVector* next = new BitVector(*((*out)[&bb]));
           
-          // temporary variables for convenience
+          // Temporary variables for convenience
           BitVector* instVec = next; // for empty blocks
           Instruction* inst;
 
-          // add local phi-captured nodes to out          
-       //   for (BasicBlock::iterator ii = bb.begin(), ib = bb.end(); ii != ib; ++ii) {
-       //     if (isa<PHINode>(*ii)) {
-       //       PHINode* phiInst = cast<PHINode>(&*ii);
-       //       unsigned idx = phiInst->  getBasicBlockIndex (&bb);
-       //       if (idx < phiInst->getNumIncomingValues()){
-       //         Value* v = phiInst -> getIncomingValue(idx);
-       //         if (isa<Instruction>(v) || isa<Argument>(v))
-       //         {
-       //           (*next)[(*index)[v]] = true;
-       //         }
-       //       }
-       //     }
-       //   }
 
-          // go through instructions in reverse
+          // Go through instructions in reverse
           BasicBlock::iterator ii = --(bb.end()), ib = bb.begin();
           while (true) {
             
-            // inherit data from next instruction
+            // Inherit data from next instruction
             inst = &*ii;
             instVec = (*instIn)[inst];            
             *instVec = *next;
             
-            // if this instruction is a new definition, remove it
-            //if (isDefinition(inst))
-            //  (*instVec)[(*index)[inst]] = true;
-            //
-                            
-            // add the arguments, unless it is a phi node
-            //if (!isa<PHINode>(*ii)) {
-            //
             if (!isa<StoreInst>(inst) && (isa<CallInst>(inst)|| isa<TerminatorInst>(inst) 
                 || (!(*instVec)[(*index)[inst]]))) {
+              // This instruction is either a call, terminator, or some instruction with a non-faint LHS
+              // In this case, we must mark all variables used in the instruction as non-faint
+              // For function calls, the arguments to the function are not faint (and the function call cannot be eliminated) even if its return value is never used because the function might have side effects.
               User::op_iterator OI, OE;
               for (OI = inst->op_begin(), OE=inst->op_end(); OI != OE; ++OI) {
                 if (isa<Instruction>(*OI) || isa<Argument>(*OI)) {
@@ -159,6 +139,8 @@ namespace
                 }
               }
             } else if (isa<StoreInst>(inst)) {
+              // For stores, mark stored value as not-faint iff the destination of a store is locally allocated and not-faint, OR if it is a global/argument
+              // In case of stores to a global/argument, the stored value can never be faint since it may be used outside the function.
               StoreInst* si = cast<StoreInst>(inst);
               Value * addr = si->getPointerOperand();
 
@@ -168,42 +150,34 @@ namespace
                   (*instVec)[(*index)[val]] = false;
               }
             }
-            //}
             next = instVec;
-
 
             if (ii == ib) break;
             --ii;
           }
           
-          // remove the phi nodes from in 
           instVec = new BitVector(*instVec);
-       //   for (BasicBlock::iterator ii = bb.begin(), ib = bb.end(); ii != ib; ++ii) {
-       //     if (isa<PHINode>(*ii)) {
-       //       PHINode* phiInst = cast<PHINode>(&*ii);
-       //       unsigned idx = phiInst->  getBasicBlockIndex (&bb);
-       //       if (idx < phiInst->getNumIncomingValues()){
-       //         Value* v = phiInst -> getIncomingValue(idx);
-       //         if (isa<Instruction>(v) || isa<Argument>(v))
-       //         {
-       //           (*next)[(*index)[v]] = false;
-       //         }
-       //       }
-       //     }
-       //   }
           return instVec;
         }
 
         // Dead Code Elimination. Removes all instructions that create/store to faint variables
-        // Will never remove function calls as these might have side effects
+        // Do not remove:
+        // - Function Calls
+        // - Stores to global variables or arguments
+        // Such instructions cannot be removed as they might have side effects
         virtual bool Eliminate(Function &F) {
-          bool modified = false; //did we actually change anything? LLVM needs this info
+          //did we actually change anything? LLVM needs this info
+          bool modified = false; 
+
+          //assumes the FVA analysis has already been completed
           BitVector *faint = (*in)[&(F.getEntryBlock())];
 
+          //Iterate over all instructions in the function, and remove ones that define faint variables
           inst_iterator ii = inst_begin(F);
 
           while (ii != inst_end(F)) {
             if (isEliminableDef(&*ii) && ((*faint)[(*index)[&*ii]])) {
+              // Instruction is not a function call, terminator or store
               inst_iterator j = ii;
               ++ii;
               j->eraseFromParent();
@@ -211,61 +185,23 @@ namespace
             } else if (isa<StoreInst>(&*ii)) {
               Value * addr = cast<StoreInst>(&*ii)->getPointerOperand();
               if (isa<AllocaInst>(addr) && (*faint)[(*index)[addr]]) {
+                //make sure store is to a variable allocated within this function
                 inst_iterator j = ii;
                 ++ii;
                 j->eraseFromParent();
                 modified = true;
               } else {
+                //Do not remove stores to a global variable or arguments
                 ++ii;
               }
             } else {
+              //Do not remove function calls/terminators.
               ++ii;
             }
           }
 
-
           return modified;
         }
-        
-        virtual void displayResults(Function &F) {
-          // iterate over basic blocks
-          Function::iterator bi = F.begin(), be = (F.end());
-          printBV( (*out)[&*bi] ); // entry node
-          for (; bi != be; ) {            
-            errs() << bi->getName() << ":\n"; //Display labels for basic blocks
-          
-            // iterate over remaining instructions except very first one
-            BasicBlock::iterator ii = bi->begin(), ie = (bi->end());
-            errs() << "\t" << *ii << "\n";
-            for (ii++; ii != ie; ii++) {
-              if (!isa<PHINode>(*(ii))) {
-                printBV( (*instIn)[&*ii] );
-              }
-              errs() << "\t" << *ii << "\n";
-            }
-            
-            // display in[bb]
-            ++bi;
-            
-            if (bi != be && !isa<PHINode>(*((bi)->begin())))
-              printBV( (*out)[&*bi] );
-
-            errs() << "\n";
-          }
-          printBV( (*out)[&*(--bi)] );
-        }
-        
-        virtual void printBV(BitVector *bv) {
-          errs() << "{ ";
-          for (int i=0; i < numTotal; i++) {
-            if ( (*bv)[i] ) {
-              WriteAsOperand(errs(), (*r_index)[i], false);
-              errs() << " ";
-            }
-          }
-          errs() << "}\n";
-        }
-    
     };
 
     char DCE::ID = 0;
